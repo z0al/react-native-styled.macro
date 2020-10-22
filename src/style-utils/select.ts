1f6e4002ccf56c84d13be6545d3d25da472edbf1
Ahmed T. Ali <ah.tajelsir@gmail.com>
2020-10-22 14:58:53 +0200

    fix(style-utils): incorrect typing

--- src/style-utils/select.ts
+++ src/style-utils/select.ts
@@ -1,11 +1,11 @@
 // Ours
-import { StyledToken, VariantStyle, ToggleVariants } from '../types';
+import { StyleProps, VariantStyle, ToggleVariants } from '../types';
 import { builtin } from '../variants';
 
 export const select = (
 	variantStyles: VariantStyle[],
 	variants: ToggleVariants = builtin
-): StyledToken[] => {
+): StyleProps[] => {
 	// Override built-in variants with user provided values but always
 	// include default variant styles
 	variants = { ...builtin, ...variants, default: true };
