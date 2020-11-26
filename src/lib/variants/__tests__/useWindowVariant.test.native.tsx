39b8f347f7bf86f91d467e5e7921559d897f60c6
Ahmed T. Ali <ah.tajelsir@gmail.com>
2020-11-26 14:03:23 +0100

    test: use jest/universal preset

--- src/lib/variants/__tests__/useWindowVariant.test.native.tsx
+++ src/lib/variants/__tests__/useWindowVariant.test.native.tsx
@@ -1,7 +1,10 @@
 // Packages
-import { render, act } from '@testing-library/react-native';
 import React from 'react';
 import { Text, Dimensions } from 'react-native';
+import { render, act } from '@testing-library/react-native';
+import * as nativeMatchers from '@testing-library/jest-native';
+
+expect.extend(nativeMatchers);
 
 // Ours
 import { useWindowVariant, BREAKPOINTS } from '../useWindowVariant';
@@ -22,39 +25,44 @@ test('useWindowVariant', () => {
 	act(() => {
 		setWidth(BREAKPOINTS.sm - 1);
 	});
+
 	rerender(<TestComponent />);
-	// @ts-expect-error
+
 	expect(getByTestId('text')).toHaveTextContent('{}');
 
 	// sm
 	act(() => {
 		setWidth(BREAKPOINTS.md - 1);
 	});
+
 	rerender(<TestComponent />);
-	// @ts-expect-error
+
 	expect(getByTestId('text')).toHaveTextContent('{"sm":true}');
 
 	// md
 	act(() => {
 		setWidth(BREAKPOINTS.lg - 1);
 	});
+
 	rerender(<TestComponent />);
-	// @ts-expect-error
+
 	expect(getByTestId('text')).toHaveTextContent('{"md":true}');
 
 	// lg
 	act(() => {
 		setWidth(BREAKPOINTS.xl - 1);
 	});
+
 	rerender(<TestComponent />);
-	// @ts-expect-error
+
 	expect(getByTestId('text')).toHaveTextContent('{"lg":true}');
 
 	// xl
 	act(() => {
 		setWidth(BREAKPOINTS.xl);
 	});
+
 	rerender(<TestComponent />);
-	// @ts-expect-error
+
 	expect(getByTestId('text')).toHaveTextContent('{"xl":true}');
 });
