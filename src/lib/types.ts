e66b58221389bc3d7cf1aa1ed2b008df8fcadcb1
Ahmed T. Ali <ah.tajelsir@gmail.com>
2020-11-29 15:37:33 +0100

    feat: improve typing 🔥

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,24 +1,56 @@
-// @noflow
+// Packages
+import { UnionToIntersection, ValueOf, LiteralUnion } from 'type-fest';
 
 // Ours
 import { Theme } from './theme';
+import createStyles from './style';
+import createProps from './style/props';
+import { builtinVariants } from './variants';
 
-export type StyleProps = {
-	style: Record<string, any> | Record<string, any>[];
-	[props: string]: unknown;
-};
-
-export type VariantStyle = StyleProps & {
+export type TokenInfo = {
 	variant: string;
+	style: Record<string, any>;
+	[props: string]: unknown;
 };
 
 export type VariantFlags = Record<string, boolean | undefined>;
 
-export type StyledMacro = (
-	tokens: string[],
-	variants?: VariantFlags
-) => StyleProps;
-
 export type Configuration = {
 	theme: Theme;
 };
+
+type Styles = UnionToIntersection<ReturnType<typeof createStyles>>;
+type Props = UnionToIntersection<ReturnType<typeof createProps>>;
+
+type Variant = LiteralUnion<
+	keyof UnionToIntersection<typeof builtinVariants>,
+	string
+>;
+
+type Token = keyof UnionToIntersection<Styles | Props>;
+
+type TokenWithVariant =
+	| Token
+	| `${Variant}:${Token}`
+	| `${Variant}:${Variant}:${Token}`;
+
+type ExtractToken<T extends string> = T extends `${string}:${infer V}`
+	? V extends Token
+		? V
+		: ExtractToken<V>
+	: T extends Token
+	? T
+	: never;
+
+type ComputedStyle<T extends Token> = UnionToIntersection<
+	T extends keyof Styles
+		? { style: ValueOf<Styles, T>[] }
+		: never | T extends keyof Props
+		? ValueOf<Props, T>
+		: never
+>;
+
+export type Macro = <T extends TokenWithVariant>(
+	tokens: T[],
+	variants?: VariantFlags
+) => ComputedStyle<ExtractToken<T>>;
